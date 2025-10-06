0a380bf5666d32b359d7ca5e0022d30d5afff21c
Matko Manzin <mmanzin@vub.hr>
2025-10-06 22:47:03 +0200

    Update TCP_EchoServer.py
    Modified TCP server to continue running instead of closing

--- LV1/TCP_EchoServer.py
+++ LV1/TCP_EchoServer.py
@@ -6,13 +6,19 @@ PORT = 63444
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
     s.bind((HOST, PORT))
     s.listen()
-    conn, addr = s.accept()
 
-    with conn:
-        print("Connected by", addr)
+    while True:
+        conn, addr = s.accept()
+        with conn:
+            print("Connected by", addr)
 
-        while True:
-            data = conn.recv(1024)
-            if not data:
-                break
-            conn.sendall(data)
+            while True:
+                data = conn.recv(1024)
+                if not data:
+                    break
+
+                conn.sendall(data)
+
+        answer = input("Continue running the server? [Y/N]: ")
+        if answer.upper() != "Y":
+            break
